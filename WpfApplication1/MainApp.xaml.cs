03fc3ea9b0406e598c7351dc51532a6c5e8cb0a2
Sen Wang <moli.wang@gmail.com>
2013-05-22 12:00:27 +0800

    add Hand gesture recognition

--- WpfApplication1/MainApp.xaml.cs
+++ WpfApplication1/MainApp.xaml.cs
@@ -28,8 +28,7 @@ namespace WpfApplication1
             result.IsPressTarget = true;
             result.PressAttractionPointX = 0.5;
             result.PressAttractionPointY = 0.5;
-            result.PressTargetControlId = 1;
-     
+            result.PressTargetControlId = 1;     
             return result;
         }
     }
@@ -38,13 +37,9 @@ namespace WpfApplication1
     {
         KinectSensor kinect;
         InteractionStream interStream;
-        Skeleton[] skeletons; 
-        UserInfo[] userInfos; 
         public MainApp(KinectSensor sensor) : this()
         {
             kinect = sensor;
-            skeletons = new Skeleton[kinect.SkeletonStream.FrameSkeletonArrayLength];
-            userInfos = new UserInfo[InteractionFrame.UserInfoArrayLength];
 
             kinect.DepthStream.Range = DepthRange.Near;
             kinect.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
@@ -64,22 +59,108 @@ namespace WpfApplication1
         {
             InitializeComponent();
         }
-
+       
+        
         void interStream_InteractionFrameReady(object sender, InteractionFrameReadyEventArgs e)
         {
             using (var iaf = e.OpenInteractionFrame())
             {
+                #region 標準處理架構
                 if (iaf == null)
                     return;
 
+                UserInfo[] userInfos = new UserInfo[InteractionFrame.UserInfoArrayLength];
                 iaf.CopyInteractionDataTo(userInfos);
+                #endregion
 
-                users.Text = "找到的使用者ID有: ";
-                foreach (var userInfo in userInfos)
+                #region 擷取互動串流提供的資訊
+                GetIDs(userInfos);
+
+                var activeuser = (from u in userInfos
+                                  where u.SkeletonTrackingId > 0
+                                  select u).FirstOrDefault();
+
+                if (activeuser != null)
                 {
-                    if( userInfo.SkeletonTrackingId > 0 )
-                        users.Text += userInfo.SkeletonTrackingId + ",";
+                    GetHands(activeuser);
+                    GetHandEvent(activeuser);
                 }
+                #endregion
+            }
+        }
+
+        private void GetIDs(UserInfo[] userInfos)
+        {
+            users.Text = "使用者IDs: ";
+            foreach (var userInfo in userInfos)
+            {
+                if (userInfo.SkeletonTrackingId > 0)
+                    users.Text += userInfo.SkeletonTrackingId + "  ";
+            }
+        }
+
+        private void GetHands(UserInfo userInfo)
+        {
+            rhand.Text = "沒有發現使用者右手";
+            lhand.Text = "沒有發現使用者左手";
+            var hands = userInfo.HandPointers;
+            if (hands.Count > 0)
+            {               
+                foreach (var hand in hands)
+                {
+                    if (hand.HandType == InteractionHandType.Right)
+                    {
+                        rhand.Text = FormatHandData(hand);
+                    }
+                    else if (hand.HandType == InteractionHandType.Left)
+                    {
+                        lhand.Text = FormatHandData(hand);
+                    }
+                }
+            }
+        }
+
+        private static string FormatHandData(InteractionHandPointer hand)
+        {
+            string rawxloc = String.Format("X座標:{0:0.0}",hand.RawX) ;
+            string rawyloc = String.Format(",Y座標:{0:0.0}", hand.RawY);
+            string rawzloc = String.Format(",Z座標:{0:0.0}", hand.RawZ);
+            //string xloc = String.Format(",校正X座標:{0:0.0}", hand.X);
+            //string yloc = String.Format(",校正Y座標:{0:0.0}", hand.Y);
+
+            return rawxloc + rawyloc + rawzloc;
+        }
+
+        private void GetHandEvent(UserInfo userInfo)
+        {
+            var hands = userInfo.HandPointers;
+            if (hands.Count > 0)
+            {
+                foreach (var hand in hands)
+                {
+                    ParseHandEvent(hand);
+                }
+            }
+        }
+
+        private void ParseHandEvent(InteractionHandPointer hand)
+        {
+            if (hand.HandEventType == InteractionHandEventType.None)
+                return;
+
+            if (hand.HandType == InteractionHandType.Right)
+            {
+                if (hand.HandEventType == InteractionHandEventType.Grip)
+                    status.Text = "右手握拳";
+                else
+                    status.Text = "右手張開";
+            }
+            else if (hand.HandType == InteractionHandType.Left)
+            {
+                if (hand.HandEventType == InteractionHandEventType.Grip)
+                    status.Text = "左手握拳";
+                else
+                    status.Text = "左手張開";
             }
         }
 
@@ -87,14 +168,13 @@ namespace WpfApplication1
         {
             using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
             {
-
                 if (depthFrame == null)
                     return;
 
                 interStream.ProcessDepth(depthFrame.GetRawPixelData(), depthFrame.Timestamp);
             }
         }
-
+       
         void kinect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
             using (SkeletonFrame skf = e.OpenSkeletonFrame())
@@ -102,14 +182,12 @@ namespace WpfApplication1
                 if (skf == null)
                     return;
 
+                Skeleton[] skeletons = new Skeleton[skf.SkeletonArrayLength];
                 skf.CopySkeletonDataTo(skeletons);
                 var accelerometerReading = kinect.AccelerometerGetCurrentReading();
                 interStream.ProcessSkeleton(skeletons, accelerometerReading, skf.Timestamp);
             }
         }
         
-
-
-
     }
 }
